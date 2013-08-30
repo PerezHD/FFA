50012608dc3886e7e4da7f944e6c968e08bd5490
Harry5573OP <devane1249@gmail.com>
2013-08-30 06:32:06 +0100

    Major Recode

--- src/com/harry5573/ffa/killstreaks/Killstreak.java
+++ src/com/harry5573/ffa/killstreaks/Killstreak.java
@@ -1,35 +1,20 @@
 package com.harry5573.ffa.killstreaks;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.Sound;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.FireworkMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import com.harry5573.ffa.Main;
 
 public class Killstreak implements Listener {
-
-    String prefix = ChatColor.GOLD + "[FFA]:";
-    String prefix2 = ChatColor.YELLOW + "[" + ChatColor.RED + "FFA" + ChatColor.YELLOW + "]";
     
     Main plugin;
 
@@ -40,162 +25,62 @@ public class Killstreak implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onDeath(PlayerDeathEvent event) {
-        //WEAPONS
-        ItemStack sworddiamond = new ItemStack(Material.DIAMOND_SWORD, 1);
-        ItemStack axediamond = new ItemStack(Material.DIAMOND_AXE, 1);
-        sworddiamond.addEnchantment(Enchantment.DURABILITY, 3);
-        sworddiamond.addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        axediamond.addEnchantment(Enchantment.DURABILITY, 3);
-        axediamond.addEnchantment(Enchantment.DAMAGE_ALL, 1);
-
-        ItemMeta meta1 = axediamond.getItemMeta();
-        List loreData1 = new ArrayList();
-        loreData1.add(ChatColor.BLUE + "FFA Item");
-        meta1.setDisplayName(ChatColor.GREEN + "FFA Axe");
-        meta1.setLore(loreData1);
-        axediamond.setItemMeta(meta1);
-
-        ItemMeta meta7 = sworddiamond.getItemMeta();
-        List loreData7 = new ArrayList();
-        loreData7.add(ChatColor.BLUE + "FFA Item");
-        meta7.setDisplayName(ChatColor.GREEN + "FFA Sword");
-        meta7.setLore(loreData7);
-        sworddiamond.setItemMeta(meta7);
-        //New Bow For Player After 3 Kills
-        ItemStack bow = new ItemStack(Material.BOW);
-        bow.addEnchantment(Enchantment.ARROW_DAMAGE, 1);
-        bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
-        ItemStack arrow = new ItemStack(Material.ARROW, 1);
-        //New Armour
-        ItemStack helmet = new ItemStack(Material.IRON_HELMET, 1);
-        ItemStack chestplate = new ItemStack(Material.IRON_CHESTPLATE, 1);
-        ItemStack leggings = new ItemStack(Material.IRON_LEGGINGS, 1);
-        ItemStack boots = new ItemStack(Material.IRON_BOOTS, 1);
-        helmet.addEnchantment(Enchantment.DURABILITY, 3);
-        helmet.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
-        chestplate.addEnchantment(Enchantment.DURABILITY, 3);
-        chestplate.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
-        leggings.addEnchantment(Enchantment.DURABILITY, 3);
-        leggings.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
-        boots.addEnchantment(Enchantment.DURABILITY, 3);
-        boots.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
-        //Name Armour
-        ItemMeta meta2 = helmet.getItemMeta();
-        List loreData2 = new ArrayList();
-        loreData2.add(ChatColor.BLUE + "FFA Item");
-        meta2.setDisplayName(ChatColor.GREEN + "FFA Helmet");
-        meta2.setLore(loreData2);
-        helmet.setItemMeta(meta2);
-
-        ItemMeta meta3 = chestplate.getItemMeta();
-        List loreData3 = new ArrayList();
-        loreData3.add(ChatColor.BLUE + "FFA Item");
-        meta3.setDisplayName(ChatColor.GREEN + "FFA Chestplate");
-        meta3.setLore(loreData3);
-        chestplate.setItemMeta(meta3);
-
-        ItemMeta meta4 = leggings.getItemMeta();
-        List loreData4 = new ArrayList();
-        loreData4.add(ChatColor.BLUE + "FFA Item");
-        meta4.setDisplayName(ChatColor.GREEN + "FFA Leggings");
-        meta4.setLore(loreData4);
-        leggings.setItemMeta(meta4);
-
-        ItemMeta meta5 = boots.getItemMeta();
-        List loreData5 = new ArrayList();
-        loreData5.add(ChatColor.BLUE + "FFA Item");
-        meta5.setDisplayName(ChatColor.GREEN + "FFA Boots");
-        meta5.setLore(loreData5);
-        boots.setItemMeta(meta5);
-
         Entity e = event.getEntity();
         if ((e instanceof Player) && (plugin.killstreak.containsKey((Player) e))) {
             Player killed = event.getEntity();
-            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tptoggle " + killed.getName() + " on");
+
             event.getDrops().removeAll(event.getDrops());
+            
             Player killer = ((Player) e).getKiller();
+            
             if (killer != null) {
+                
+                
                 int x = ((Integer) plugin.killstreak.get(killer)).intValue();
                 event.setDroppedExp(0);
-                event.setDeathMessage("");
+                event.setDeathMessage(null);
                 int newx = x + 1;
+                
                 plugin.killstreak.remove(killer);
                 plugin.killstreak.put(killer, Integer.valueOf(newx));
                 killer.giveExpLevels(1);
+                
                 //Give new potions :)
-                plugin.givePotions(killer);
+                plugin.ih.givePotions(killer);
                 if (newx == 1) {
-                    killer.getInventory().setItem(0, sworddiamond);
-                    killer.getInventory().setItem(1, axediamond);
-                    killer.sendMessage(prefix + ChatColor.GRAY + " Your Weapons Have Been Upgraded!");
-                } else if (newx == 3) {
-                    killer.getInventory().setItem(6, bow);
-                    killer.getInventory().setItem(7, arrow);
+                    plugin.ih.addDiamondTools(killer);
+                    killer.sendMessage(plugin.prefix + ChatColor.GRAY + " Your Weapons Have Been Upgraded!");
+                } else if ((newx == 3) || (newx == 6) || (newx == 9) || (newx == 15) || (newx == 18) || (newx == 21) || (newx == 27) || (newx == 30) || (newx == 33) || (newx == 39) || (newx == 42) || (newx == 45) |(newx == 51) || (newx == 54)) {
+                    plugin.ih.giveBow(killer);
                     Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
                     killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 3 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
-                } else if (newx == 6) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 6 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
-                } else if (newx == 9) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 9 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
+                    Bukkit.broadcastMessage(plugin.prefix + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a " + ChatColor.AQUA + newx + ChatColor.GRAY + " killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
                 } else if (newx == 12) {
                     Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 2500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 12 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$2500 " + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "New Armour" + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "Speed 3");
-                    killer.getInventory().setHelmet(helmet);
-                    killer.getInventory().setChestplate(chestplate);
-                    killer.getInventory().setLeggings(leggings);
-                    killer.getInventory().setBoots(boots);
-                    killer.sendMessage(prefix + ChatColor.YELLOW + " You have hit a 12 killstreak and have received new armor and speed 3");
-                    killer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 3000000, 2));
-                } else if (newx == 15) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 15 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
-                } else if (newx == 18) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 18 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
-                } else if (newx == 21) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 21 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
+                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);    
+                    plugin.ih.giveArmour(killer);
+                    killer.sendMessage(plugin.prefix + ChatColor.YELLOW + " You have hit a 12 killstreak and have received new armor and speed 3");
+                    killer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 2));
                 } else if (newx == 24) {
                     Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 5000");
                     killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 24 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$5000 " + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "New Armour" + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "Strength");
-                    killer.getInventory().setHelmet(helmet);
-                    killer.getInventory().setChestplate(chestplate);
-                    killer.getInventory().setLeggings(leggings);
-                    killer.getInventory().setBoots(boots);
-                    killer.sendMessage(prefix + ChatColor.YELLOW + " You have hit a 24 killstreak and have received new armour and strength");
-                    killer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 3000000, 0));
-                } else if (newx == 27) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 27 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
-                } else if (newx == 30) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 30 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
-                } else if (newx == 33) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 500");
-                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 33 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$500");
+                    Bukkit.broadcastMessage(plugin.prefix + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 24 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$5000 " + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "New Armour" + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "Strength");
+                    plugin.ih.giveArmour(killer);
+                    killer.sendMessage(plugin.prefix + ChatColor.YELLOW + " You have hit a 24 killstreak and have received new armour and strength");
+                    killer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, 0));
                 } else if (newx == 36) {
                     Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 7500");
                     killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
-                    Bukkit.broadcastMessage(prefix2 + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 36 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$7500     " + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "New Armour" + ChatColor.GRAY + " + " + ChatColor.DARK_RED + "Regeneration");
-                    killer.getInventory().setHelmet(helmet);
-                    killer.getInventory().setChestplate(chestplate);
-                    killer.getInventory().setLeggings(leggings);
-                    killer.getInventory().setBoots(boots);
-                    killer.sendMessage(prefix + ChatColor.YELLOW + " You have hit a 36 killstreak and have received new armour and regeration");
-                    killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 3000000, 0));
+                    Bukkit.broadcastMessage(plugin.prefix + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 36 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$7500     " + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "New Armour" + ChatColor.GRAY + " + " + ChatColor.DARK_RED + "Regeneration");
+                    plugin.ih.giveArmour(killer);
+                    killer.sendMessage(plugin.prefix + ChatColor.YELLOW + " You have hit a 36 killstreak and have received new armour and regeration");
+                    killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 0));
+                } else if (newx == 48) {
+                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "eco give " + killer.getName() + " 7500");
+                    killer.playSound(killer.getLocation(), Sound.GHAST_SCREAM, 5.0F, 1.0F);
+                    Bukkit.broadcastMessage(plugin.prefix + ChatColor.DARK_RED + " " + killer.getName() + ChatColor.GRAY + " Is on a" + ChatColor.AQUA + " 36 " + ChatColor.GRAY + "killstreak and has recived " + ChatColor.LIGHT_PURPLE + "$7500     " + ChatColor.GRAY + " + " + ChatColor.LIGHT_PURPLE + "New Armour" + ChatColor.GRAY + " + " + ChatColor.DARK_RED + "Regeneration");
+                    plugin.ih.giveArmour(killer);
+                    killer.sendMessage(plugin.prefix + ChatColor.YELLOW + " You have hit a 48 killstreak and have received new armour");
                 }
             }
         }
---
+++ src/com/harry5573/ffa/listeners/EventListener.java
@@ -0,0 +1,177 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.harry5573.ffa.listeners;
+
+import com.harry5573.ffa.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+/**
+ *
+ * @author Harry5573
+ */
+public class EventListener implements Listener {
+
+    public static Main plugin;
+
+    public EventListener(Main instance) {
+        this.plugin = instance;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onBreak(BlockBreakEvent e) {
+        Player p = e.getPlayer();
+        if (plugin.killstreak.containsKey(p)) {
+            e.setCancelled(true);
+            p.sendMessage(plugin.prefix + ChatColor.GREEN + " You may not break blocks in ffa.");
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlace(BlockPlaceEvent e) {
+        Player p = e.getPlayer();
+        if (plugin.killstreak.containsKey(p)) {
+            e.setCancelled(true);
+            p.sendMessage(plugin.prefix + ChatColor.GREEN + " You may not place blocks in ffa.");
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        Player p = event.getPlayer();
+        if (plugin.killstreak.containsKey(p)) {
+            p.sendMessage(plugin.prefix + ChatColor.RED + " You can't use commands while in FFA!");
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void inventoryFFADrop(PlayerDropItemEvent event) {
+        Player p = event.getPlayer();
+        if (plugin.killstreak.containsKey(p)) {
+            event.setCancelled(true);
+            p.sendMessage(plugin.prefix + ChatColor.RED + " You are not allowed to drop items in FFA.");
+            p.updateInventory();
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onFFAOpenEvent(InventoryOpenEvent e) {
+        Player p = (Player) e.getPlayer();
+        if (!(plugin.killstreak.containsKey(p))) {
+            return;
+        }
+        if (e.getInventory().getType() == InventoryType.CHEST) {
+            e.setCancelled(true);
+        }
+        if (e.getInventory().getType() == InventoryType.WORKBENCH) {
+            e.setCancelled(true);
+        }
+        if (e.getInventory().getType() == InventoryType.ANVIL) {
+            e.setCancelled(true);
+        }
+        if (e.getInventory().getType() == InventoryType.BREWING) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onFFAPickupItem(PlayerPickupItemEvent event) {
+        Player p = event.getPlayer();
+        if (plugin.killstreak.containsKey(p)) {
+            event.setCancelled(true);
+            p.updateInventory();
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onMove(PlayerMoveEvent event) {
+        Player p = event.getPlayer();
+
+        if (plugin.timer.containsKey(p)) {
+            if ((event.getFrom().getBlockX() != event.getTo().getBlockX())
+                    || (event.getFrom().getBlockZ() != event.getTo().getBlockZ())) {
+                Bukkit.getScheduler().cancelTask(((Integer) plugin.timer.get(p)).intValue());
+                plugin.timer.remove(p);
+                event.getPlayer().sendMessage(plugin.prefix + ChatColor.RED + " Teleport cancelled due to player movement.");
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onFFAQuit(PlayerQuitEvent event) {
+        Player p = event.getPlayer();
+        if ((plugin.killstreak.containsKey(p))) {
+            plugin.removeFromFFA(p);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onKick(PlayerKickEvent event) {
+        Player p = event.getPlayer();
+        if ((plugin.killstreak.containsKey(p))) {
+            plugin.removeFromFFA(p);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void respawn(PlayerRespawnEvent event) {
+        Player p = event.getPlayer();
+        if ((plugin.killstreak.containsKey(p))) {
+            plugin.respawnFFA(p);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+
+        if (plugin.killstreak.containsKey(player)) {
+            if (event.getSlotType() == InventoryType.SlotType.ARMOR) {
+                event.setCancelled(true);
+                player.sendMessage(plugin.prefix + ChatColor.GREEN + " You cant remove your armour in FFA");
+            }
+            if (event.getSlot() == 0) {
+                event.setCancelled(true);
+                player.sendMessage(plugin.prefix + ChatColor.GREEN + " You cant move your sword/axe in FFA");
+            }
+            if (event.getSlot() == 1) {
+                event.setCancelled(true);
+                player.sendMessage(plugin.prefix + ChatColor.GREEN + " You cant move your sword/axe in FFA");
+            }
+        }
+    }
+
+    //Stop them from teleporting
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onTeleport(PlayerTeleportEvent event) {
+        Player player = event.getPlayer();
+        {
+            if ((event.getCause().equals(PlayerTeleportEvent.TeleportCause.COMMAND)) || (event.getCause().equals(PlayerTeleportEvent.TeleportCause.PLUGIN))) {
+                if (plugin.killstreak.containsKey(player)) {
+                    event.setCancelled(true);
+                    player.sendMessage(plugin.prefix + ChatColor.LIGHT_PURPLE + " You cannot teleport in ffa");
+                }
+            }
+        }
+    }
+}
